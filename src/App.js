dc0abe02057c59620a4b3ce3aa226ee132e1c4ed
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-05-04 15:47:04 +0100

    Add images folder, update alt text and reduce main cover image size

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
 import React from "react";
-import ET from "./et.jpg";
-import Ozark from "./ozark.jpg";
-import TigerKing from "./tigerking.jpg";
-import MoneyHeist from "./MoneyHeist.jpg";
-import Got from "./got.jpg";
-import BreakingBad from "./breakingbad.jpg";
-import Narcos from "./narcos.jpg";
-
+import Et from "./images/et.jpg";
+import Ozark from "./images/ozark.jpg";
+import TigerKing from "./images/tigerking.jpg";
+import MoneyHeist from "./images/MoneyHeist.jpg";
+import Got from "./images/got.jpg";
+import BreakingBad from "./images/breakingbad.jpg";
+import Narcos from "./images/narcos.jpg";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <img src={ET} alt="ET movie" />
+      <img src={Et} alt="ET movie" />
       <br /> <br /> <br /> <br />
       <div className="container">
         <div className="item">
-          <img src={Ozark} alt="ET movie" />
+          <img src={Ozark} alt="Ozark cover" />
         </div>
         <div className="item">
-        <img src={TigerKing} alt="ET movie" />
+          <img src={TigerKing} alt="Tiger King cover" />
         </div>
         <div className="item">
-        <img src={MoneyHeist} alt="ET movie" />
+          <img src={MoneyHeist} alt="MoneyHeist cover" />
         </div>
       </div>
       <div className="container">
         <div className="item">
-        <img src={Got} alt="ET movie" />
+          <img src={Got} alt="G.O.T cover" />
         </div>
         <div className="item">
-        <img src={BreakingBad} alt="ET movie" />
+          <img src={BreakingBad} alt="Breaking Bad cover" />
         </div>
         <div className="item">
-        <img src={Narcos} alt="ET movie" />
+          <img src={Narcos} alt="Narcos cover" />
         </div>
       </div>
     </div>
