ad413963216a60c701ab124b92c5922a23fecc9a
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-05-27 10:57:10 +0100

    Introduce a back-end server and return list of shows from the backend

---
+++ src/backend/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const gallery = require('./gallery')
+const app = express()
+const port = 3000
+
+app.get('/rest/shows/', (req, res) => res.send(gallery.arrayTvShows()));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
