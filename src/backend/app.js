f2a87ce329c46dcce2e576d198bc28a3a5d13f18
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-06-02 12:31:19 +0100

    Enable the backend server in prod

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const gallery = require('./gallery')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'../../build')));
 
 app.get('/rest/shows/', (req, res) => res.send(gallery.arrayTvShows()));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
