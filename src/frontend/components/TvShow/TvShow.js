f0afcf5afc3b1e841c031aac8205254fd2da43f9
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-06-09 11:19:28 +0100

    Refactor code TvShows component

--- src/frontend/components/TvShow/TvShow.js
+++ src/frontend/components/TvShow/TvShow.js
@@ -4,10 +4,10 @@ import "./TvShow.css"
 
 export default function TvShow(props) {
   return (
-    <Link to={"/" + props.id} className="link-movie">
-      <div className="item">
+    <Link to={"/" + props.id} className="TvShow">
+      <div className="TvShow-item">
         <img src={props.picture} alt={props.title + " cover"} />
-        <div className="overlay-title">
+        <div className="TvShow-overlay-title">
           <h2>{props.title}</h2>
         </div>
       </div>
