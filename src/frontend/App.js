731fd4fc8a14e0ae69101d6178080628afd183f6
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-06-30 15:45:26 +0100

    Add google analytics

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import ReactGA from "react-ga";
+import React, {useEffect} from "react";
 import GalleryShows from "./components/GalleryShows/GalleryShows.js";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -7,7 +8,11 @@ import NotFound from "./components/NotFound/NotFound.js";
 
 function App() {
   
-
+  useEffect(() => {
+    ReactGA.initialize("UA-171302758-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+  
   return (
     <div className="App">
       <Router>
--- src/frontend/components/GalleryShows/GalleryShows.js
+++ src/frontend/components/GalleryShows/GalleryShows.js
@@ -7,6 +7,7 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 export default function GalleryShows() {
+   
   const [shows, setShows] = useState([]);
   const [resultsLoaded, setResultsLoaded] = useState(false);
 
