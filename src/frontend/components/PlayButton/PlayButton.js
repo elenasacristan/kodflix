5da4833031674a1f5a5e95413947876b147f2e4c
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-08-10 19:29:30 +0100

    Add animation on hover to the Play button

--- src/frontend/components/PlayButton/PlayButton.js
+++ src/frontend/components/PlayButton/PlayButton.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./PlayButton.css";
+import { FaPlay } from "react-icons/fa";
 
 export default function PlayButton({ showTitle }) {
-    return (
-        <Link className="PlayButton" to={`/${showTitle}/play`}>Play Trailer</Link>
-    );
+  return (
+    <Link className="PlayButton" to={`/${showTitle}/play`}>
+      <FaPlay className="icon-play" />
+    </Link>
+  );
 }
- 
-
-
