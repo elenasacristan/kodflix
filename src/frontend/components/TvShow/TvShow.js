0810fe1542243664a653fdd3b7eb9f575780ce70
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2020-08-19 11:50:18 +0100

    Display spinner while image loads

--- src/frontend/components/TvShow/TvShow.js
+++ src/frontend/components/TvShow/TvShow.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./TvShow.css";
+import Spinner from "../Spiner/Spiner.js";
+
 
 export default function TvShow(props) {
-  return (
-    <Link to={"/" + props.title} className="TvShow">
-      <div className="TvShow-item">
-        <img src={`/covers/${props.title}`} alt={props.title + " cover"} />
-        <div className="TvShow-overlay-title">
-          <h2>{props.title.split("_").join(" ")}</h2>
+  const [image, setImage] = useState("");
+  const [resultsLoaded, setResultsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch(`/covers/${props.title}`)
+      .then((data) => {
+        setImage(`/covers/${props.title}`);
+        setResultsLoaded(true);
+      });
+  }, [props.title]);
+
+  if (!resultsLoaded) {
+    return <Spinner />;
+  } else {
+    return (
+      <Link to={"/" + props.title} className="TvShow">
+        <div className="TvShow-item">
+          <img src={image} alt={props.title + " cover"} />
+          <div className="TvShow-overlay-title">
+            <h2>{props.title.split("_").join(" ")}</h2>
+          </div>
+          <div className="TvShow-overlay-hover"></div>
         </div>
-        <div className="TvShow-overlay-hover"></div>
-      </div>
-    </Link>
-  );
+      </Link>
+    );
+  }
 }
